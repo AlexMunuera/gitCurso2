497f03d7f61bb78fd9a62d973b7aa9393ac73717
pesca79 <martmariscal@hotmail.com>
2020-10-30 17:15:59 +0100

    #cambios hjechos por miguel

--- daysweek.java
+++ daysweek.java
@@ -4,13 +4,13 @@ public class daysweek {
 
         int roll = (int) (Math.random() * num) + 1;
 
-        if ( roll == 1 ) System.out.println("Mi día favorito de la semana es el \"lunes\".");
-        else if (roll == 2 ) System.out.println("Mi día favorito de la semana es el \"martes\".");
-        else if (roll == 3 ) System.out.println("Mi día favorito de la semana es el \"miercoles\".");
-        else if (roll == 4 ) System.out.println("Mi día favorito de la semana es el \"jueves\".");
-        else if (roll == 5 ) System.out.println("Mi día favorito de la semana es el \"viernes\".");
-        else if (roll == 6 ) System.out.println("Mi día favorito de la semana es el \"sábado\".");
-        else System.out.println("Mi día favorito de la semana es el \"domingo\".");
+        if ( roll == 1 ) System.out.println("Mi día favorito de la semana es el \"VIERNES\".");
+        else if (roll == 2 ) System.out.println("Mi día favorito de la semana es el \"VIERNES\".");
+        else if (roll == 3 ) System.out.println("Mi día favorito de la semana es el \"VIERNES\".");
+        else if (roll == 4 ) System.out.println("Mi día favorito de la semana es el \"VIERNES\".");
+        else if (roll == 5 ) System.out.println("Mi día favorito de la semana es el \"VIERNES\".");
+        else if (roll == 6 ) System.out.println("Mi día favorito de la semana es el \"VIERNES\".");
+        else System.out.println("Mi día favorito de la semana es el \"VIERNES\".");
 
     }
 }
